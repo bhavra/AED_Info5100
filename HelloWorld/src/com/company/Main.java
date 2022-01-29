f217fdbfbb118d81b8bbfdb7f3ba84bc728f6a0d
bhavra <bhavaniakumalla01@gmail.com>
2022-01-29 14:22:30 -0800

    Data structures updated

--- HelloWorld/src/com/company/Main.java
+++ HelloWorld/src/com/company/Main.java
@@ -3,7 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("Hello World");
+
+            boolean boolValue = true;
+            int iValue = 5;
+            char cValue = 'b';
+            float fValue = 3.45F;
+            Integer bigIntValue = 45;
+            Character ch = 'b';
+            String str = "I am a string";
+
     }
 }
