45d12357bb75e9b9d91d74d2dc6ba557086a1704
bhavra <bhavaniakumalla01@gmail.com>
2022-02-03 13:38:22 -0800

    Committing bhavs

---
+++ Assignment_1/Str_UniqueCharacters/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String str= "bhavs";
+        if(uniqueChars(str))
+            System.out.println("Given string has unique characters");
+        else
+            System.out.println("Given string has duplicate characters");
+    }
+
+    private static boolean uniqueChars(String str){
+        char[] arr = str.toCharArray();
+        for(int i=0; i<=str.length(); i++)
+        {
+            for(int j=i+1; j<str.length();j++)
+            {
+                //System.out.println("i,j="+i +j);
+                if(arr[i]==arr[j])
+                    return false;
+            }
+
+        }
+        return true;
+    }
+}
