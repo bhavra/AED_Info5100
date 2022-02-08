8abaf207ab5535c93db72a7036bb79dd5c668e12
bhavra <bhavaniakumalla01@gmail.com>
2022-02-07 16:19:18 -0800

    committing class & constructor

---
+++ Assignment_1/9.BookClass_Constructor/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Book book1 = new Book("Two States", 2456, "fiction", 400);
+        System.out.println("\n");
+        System.out.println(book1.getName());
+    }
+}
