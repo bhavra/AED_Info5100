097f0aba098269c44b2e24ff3279d2be96e50ba0
bhavra <bhavaniakumalla01@gmail.com>
2022-02-09 02:15:59 -0800

    committing ransomenote

--- Assignment_1/7.Ransomenote&Magazine/src/com/company/Main.java
+++ Assignment_1/7.Ransomenote&Magazine/src/com/company/Main.java
@@ -3,12 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-       System.out.println(construct("ransomenote", "magazine"));
-        System.out.println(construct("abcd", "aabbccd"));
-        System.out.println(construct("1234", "1123344455"));
+       System.out.println(compare("ransomenote", "magazine"));
+        System.out.println(compare("abcd", "aabbccd"));
+        System.out.println(compare("1234", "1123344455"));
     }
 
-    public static boolean construct(String ransomNote, String magazine){
+    public static boolean compare(String ransomNote, String magazine){
         int len1 = ransomNote.length();
         int len2 = magazine.length();
         if (len1 > len2)
