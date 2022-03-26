d3330da619d26f29c094f8febc673fe618eba97a
bhavra <bhavaniakumalla01@gmail.com>
2022-03-26 16:27:43 -0700

    DeepvsShallowCopy

--- MidTerm/DeepCopyShallowCopy/src/com/company/Differences.java
+++ MidTerm/DeepCopyShallowCopy/src/com/company/Differences.java
@@ -2,11 +2,20 @@ package com.company;
 
 public class Differences {
     /*
+    Shallow Copy:
+    1. pointing the reference to an already existing variable
+    2. changes made to the copied object will be reflected to the original object and vice versa
+    3. It is a default version of clone method
+    4. It is fast
+    5. It is used with objects of basic fields
+
+
     Deep Copy:
+    1. Creating a constructor passing the complete class to copy
+    2. changes made to the copied object will be not reflected to the original object and vice versa
+    3. we override the cloned object
+    4. It is slow
+    6. It is used with objects that has reference to other objects as fields
 
-
-
-
-
-     */
+*/
 }
