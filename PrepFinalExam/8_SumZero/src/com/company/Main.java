7a0573fc725d702d579227f84afb1693100ece30
bhavra <bhavaniakumalla01@gmail.com>
2022-05-04 21:36:34 -0700

    sum zero array

---
+++ PrepFinalExam/8_SumZero/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(sumZero(4)));
+        System.out.println(Arrays.toString(sumZero(7)));
+    }
+
+    public static int[] sumZero(int n){
+        int[] result = new int[n];
+        int left = 0;
+        int right = n - 1;
+        int digit = 1;
+
+        while (left < right) {
+            result[left] = digit;
+            left++;
+            result[right] = -digit;
+            right--;
+            digit++;
+        }
+        return result;
+    }
+
+}
