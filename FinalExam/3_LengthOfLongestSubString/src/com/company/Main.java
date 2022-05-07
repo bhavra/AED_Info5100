d06005aeccca4f5ee7d77027ba5dd10e665688f9
bhavra <bhavaniakumalla01@gmail.com>
2022-05-07 10:41:39 -0700

    Length of Longest substring

---
+++ FinalExam/3_LengthOfLongestSubString/src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String str1 = "bbbbb";
+        String str2 = "pwwkew";
+        System.out.println("length of the longest substring " +str1+ " : "+longestSubstring(str1));
+        System.out.println("length of the longest substring " +str2+ " : "+longestSubstring(str2));
+    }
+
+    public static int longestSubstring(String input){
+        int i=0, j=0;
+        int max=0;
+        String output = "";
+        HashSet<Character> hashset = new HashSet();
+        if(input.length()==0){
+            System.out.println("Empty String");
+        }
+        while(j<input.length()){
+            if(!hashset.contains(input.charAt(j))){
+                hashset.add(input.charAt(j));
+                j++;
+                max = Math.max(hashset.size(),max);
+            }
+            else{
+                hashset.remove(input.charAt(i));
+                i++;
+            }
+            //           if(output.length()< j-i+1){
+            //               output = input.substring(i,j);
+            //           }
+        }
+        //       System.out.println("Longest substring : "+output);
+        return max;
+    }
+
+}
--- FinalExam/4_ArraySumZero/src/com/company/Main.java
+++ FinalExam/4_ArraySumZero/src/com/company/Main.java
@@ -6,9 +6,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(sumZero(4)));
         System.out.println(Arrays.toString(sumZero(7)));
+        System.out.println(Arrays.toString(sumZero(0)));
+        System.out.println(Arrays.toString(sumZero(1)));
+        System.out.println(Arrays.toString(sumZero(-1)));
     }
 
     public static int[] sumZero(int n){
+        if(n<0){
+            System.out.println("invalid input");
+
+        }
         int[] result = new int[n];
         int left = 0;
         int right = n - 1;
