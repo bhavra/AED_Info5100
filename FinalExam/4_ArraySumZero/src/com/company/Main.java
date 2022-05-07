246892cdcf85f8d6e0328f9bd69e2f589d297e15
bhavra <bhavaniakumalla01@gmail.com>
2022-05-07 10:57:56 -0700

    Array sum zero

--- FinalExam/4_ArraySumZero/src/com/company/Main.java
+++ FinalExam/4_ArraySumZero/src/com/company/Main.java
@@ -4,18 +4,15 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(sumZero(4)));
-        System.out.println(Arrays.toString(sumZero(7)));
-        System.out.println(Arrays.toString(sumZero(0)));
-        System.out.println(Arrays.toString(sumZero(1)));
-        System.out.println(Arrays.toString(sumZero(-1)));
+        System.out.println(Arrays.toString(sumZero(5)));
+        System.out.println(Arrays.toString(sumZero(3)));
     }
 
     public static int[] sumZero(int n){
         if(n<0){
             System.out.println("invalid input");
-
         }
+
         int[] result = new int[n];
         int left = 0;
         int right = n - 1;
