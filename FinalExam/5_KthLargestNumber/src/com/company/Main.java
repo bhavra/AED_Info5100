71be49e736562a6b8def52e8324679caf695296d
bhavra <bhavaniakumalla01@gmail.com>
2022-05-07 11:08:22 -0700

    Kth largest element in an array

--- FinalExam/5_KthLargestNumber/src/com/company/Main.java
+++ FinalExam/5_KthLargestNumber/src/com/company/Main.java
@@ -4,10 +4,14 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        int[] nums = {51,7,19,12,3,8,99};
-        int k = 4;
-        System.out.println(k+"th largest element in the given array is "+findKthLargest(nums,k));
+        int[] nums1 = {3,2,1,5,6,4};
+        int k1 = 2;
+        System.out.println(k1+"nd largest element in the given array " +Arrays.toString(nums1)+ " : "+findKthLargest(nums1,k1));
+        int[] nums2 = {3,2,3,1,2,4,5,5,6};
+        int k2 = 4;
+        System.out.println(k2+"th largest element in the given array "  +Arrays.toString(nums2)+ " : "+findKthLargest(nums2,k2));
     }
+
     public static int findKthLargest(int[] nums, int k){
         PriorityQueue<Integer> minheap = new PriorityQueue<>();
         for(int i :nums){
