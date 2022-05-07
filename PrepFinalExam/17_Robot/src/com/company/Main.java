f1bc11f0fac3747ab9a9101e53b06fd26971578e
bhavra <bhavaniakumalla01@gmail.com>
2022-05-06 23:21:03 -0700

    robot

---
+++ PrepFinalExam/17_Robot/src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(robot("GL"));
+    }
+
+    public static boolean robot(String instructions){
+        int a=0, b=0;
+        int dir=0;
+        //0-N 1-E 2-S 3-W
+
+        int[][] directions = new int[][] { {1,0}, {0,1}, {-1,0}, {0,-1}};
+
+        for(int i=0 ; i<instructions.length() ; i++){
+            char ch = instructions.charAt(i);
+
+            if(ch=='G') {
+                a = a + directions[dir][0];
+                b = b + directions[dir][1];
+            }
+            else if(ch=='L') {
+                dir = (dir == 0) ? 3 : dir - 1;
+            }
+            else if(ch=='R') {
+                dir = (dir+1)%4;
+            }
+        }
+        if(a==0 && b==0){
+            return true;
+        }
+        if(dir == 0){
+            return false;
+        }
+        return true;
+    }
+}
